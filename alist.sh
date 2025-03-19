d282eb7f5b2cc13d2882c5522562a594ffae3dc8
jc-lw <baolw50@gmail.com>
2025-03-20 02:54:28 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-cd ~/domains
-mkdir -p serv00-play/alist
+cd ~/serv00-play
+mkdir -p alist
 cd serv00-play/alist/
 
 if [ -f nezhav1 ]; then
-    echo "alist 已存在，跳过下载。"
+    echo "start.sh 已存在，跳过下载。"
 else
     wget https://github.com/nezhahq/agent/releases/download/v1.7.3/nezha-agent_freebsd_amd64.zip >/dev/null 2>&1
     unzip nezha-agent_freebsd_amd64.zip >/dev/null 2>&1
-    mv start.sh alist
+    mv start.sh
     chmod 755 start.sh
     echo "下载成功"
 fi
 
-config_file="config.yml"
-
-# 检查 config.yml 文件是否已存在
-if [ -f "$config_file" ]; then
-    echo "$config_file 已存在。"
-else
-
-
-
-pkill -f alist
+pkill -f start.sh
 sleep 2
-chmod +x alist
-nohup ./alist server >/dev/null 2>&1 &
+chmod +x start.sh
+serv00-play/alist/start.sh
 
-echo "哪吒监控agent启动完成"
+echo "agent启动完成"
