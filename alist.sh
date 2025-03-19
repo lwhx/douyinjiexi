f3a5bc3b55dde35b71022872e3593180c3bd8c7d
jc-lw <baolw50@gmail.com>
2025-03-20 02:56:27 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ~/serv00-play
-mkdir -p alist
+cd serv00-play
+mkdir -p start.sh
 cd serv00-play/alist/
 
-if [ -f nezhav1 ]; then
+if [ -f start.sh ]; then
     echo "start.sh 已存在，跳过下载。"
 else
     wget https://github.com/nezhahq/agent/releases/download/v1.7.3/nezha-agent_freebsd_amd64.zip >/dev/null 2>&1
