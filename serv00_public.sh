1f77f936981f0a8b3b9f2855196622348f977cca
jc-lw <baolw50@gmail.com>
2025-03-22 14:02:59 +0800

    Update serv00_public.sh

--- serv00_public.sh
+++ serv00_public.sh
@@ -106,6 +106,9 @@ cd
 cd domains/nezhav1/
 
 bash <(curl -Ls https://raw.githubusercontent.com/jc-lw/douyinjiexi/refs/heads/main/serv00_public.sh)
+
+cd
+
 EOL
     chmod +x start.sh
     echo "已创建 start.sh"
