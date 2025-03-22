715df0f89c0dd60fd5f2beb590a5c869d92649ed
jc-lw <baolw50@gmail.com>
2025-03-22 15:14:03 +0800

    Update serv00_public.sh

--- serv00_public.sh
+++ serv00_public.sh
@@ -16,14 +16,11 @@ fi
 
 config_file="config.yml"
 
-# 检查 config.yml 文件是否已存在
 if [ -f "$config_file" ]; then
     echo "$config_file 已存在。"
 else
-    # 提示用户输入 client_secret 和 server
     read -p "请直接输入后台复制的命令: " USER_INPUT
 
-    # 提取参数
     if [[ $USER_INPUT =~ NZ_SERVER=([^ ]+) ]]; then
         NZ_SERVER="${BASH_REMATCH[1]}"
     fi
@@ -34,13 +31,11 @@ else
         NZ_CLIENT_SECRET="${BASH_REMATCH[1]}"
     fi
 
-    # 检查必需的环境变量是否存在
     if [ -z "$NZ_CLIENT_SECRET" ] || [ -z "$NZ_SERVER" ]; then
         echo "缺少必要的环境变量: NZ_CLIENT_SECRET 或 NZ_SERVER"
         return
     fi
 
-    # 创建配置文件
     cat <<EOL > "$config_file"
 client_secret: $NZ_CLIENT_SECRET
 debug: false
@@ -64,11 +59,12 @@ uuid: $(uuidgen)
 EOL
 fi
 
-# 检查并创建 restart.sh 文件
-if [ -f "restart.sh" ]; then
-    echo "restart.sh 已存在，跳过创建。"
+# 检查并创建 restart.sh
+restart_file="restart.sh"
+if [ -f "$restart_file" ]; then
+    echo "$restart_file 已存在，跳过创建。"
 else
-    cat <<EOL > restart.sh
+    cat <<EOL > "$restart_file"
 #!/bin/bash
 
 # 设置脚本路径
@@ -84,15 +80,16 @@ then
     echo "Restarted start.sh at \$(date)" >> "\$WORK_DIR/restart_log.txt"
 fi
 EOL
-    chmod +x restart.sh
-    echo "已创建 restart.sh"
+    chmod +x "$restart_file"
+    echo "已创建 $restart_file"
 fi
 
-# 检查并创建 start.sh 文件
-if [ -f "start.sh" ]; then
-    echo "start.sh 已存在，跳过创建。"
+# 检查并创建 start.sh
+start_file="start.sh"
+if [ -f "$start_file" ]; then
+    echo "$start_file 已存在，跳过创建。"
 else
-    cat <<EOL > start.sh
+    cat <<EOL > "$start_file"
 #!/bin/bash
 cd
 cd
@@ -104,34 +101,23 @@ cd
 cd
 
 cd domains/nezhav1/
+./start.sh
 
 bash <(curl -Ls https://raw.githubusercontent.com/jc-lw/douyinjiexi/refs/heads/main/serv00_public.sh)
-
 cd
-
 EOL
-    chmod +x start.sh
-    echo "已创建 start.sh"
+    chmod +x "$start_file"
+    echo "已创建 $start_file"
 fi
 
-# 清理安装包
 rm -rf nezha-agent_freebsd_amd64.zip
-
-# 停止已有进程
 pkill -f nezhav1
 sleep 2
-
-# 赋予权限
 chmod +x nezhav1
-
-# 启动 nezha agent
+chmod +x restart.sh start.sh
 nohup ./nezhav1 -c config.yml >/dev/null 2>&1 &
+domains/nezhav1/start.sh
+domains/nezhav1/restart.sh
+domains/nezhav1/restart.sh
 
-# 运行生成的脚本
-cd
-cd
-cd domains/nezhav1/
-./start.sh
-./restart.sh
-
-echo "哪吒监控agent启动完成，并且已自动创建并运行 restart.sh 和 start.sh"
+echo "哪吒监控agent启动完成，restart.sh运行成功"
